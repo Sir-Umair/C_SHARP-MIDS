a93e14055c05ea8e70a004785679ccadaafcab8b
Sir-Umair <umairsahib242@gmail.com>
2025-03-10 22:21:10 +0500

    first commit

--- loops.cs
+++ loops.cs
@@ -11,7 +11,20 @@
 // }
 
 // do-while loop
-    int i=0;
-do{ 
-    
-}
+//  int i=10;
+// do{ 
+//     Console.WriteLine(i);
+//     i--;
+// }
+// while(i>=0);
+//  FOR LOOP
+// for(int i=0;i<=20;i++){
+//     Console.Write(i+" ");
+// }
+
+//  FOR EACH LOOP 
+// int[] a={1,2,3,4,5};
+// foreach(int i in a ){
+//     Console.WriteLine(i);
+// }
+
